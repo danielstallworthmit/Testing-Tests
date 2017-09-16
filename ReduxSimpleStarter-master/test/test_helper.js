deefbb25085d01ad464d6f3eacac5cdef93fd88f
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-16 13:02:37 -0700

    Set up project folders and files using sample redux template

---
+++ ReduxSimpleStarter-master/test/test_helper.js
@@ -0,0 +1,9 @@
+
+
+// Set up environment to run browser in the command line
+
+//  Build 'renderComponent' helper to render a given react class
+
+// Build helper for simulating events
+
+// Set up chai-jquery library
